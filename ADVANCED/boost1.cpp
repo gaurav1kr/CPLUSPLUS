4fe6b77503ecdfa0a02cc063fc5a2dcaa4b4e8f6
Gaurav Kumar <sinha4545@gmail.com>
2025-01-22 19:56:13 +0530

    boost compilation

--- ADVANCED/boost1.cpp
+++ ADVANCED/boost1.cpp
@@ -1,3 +1,6 @@
+//Below program will be compiled with C++17 only. Below are the steps for it's compilation :- 
+//1. Get the boost path (It might be in /opt or /usr)
+//2. Compile it with include path - g++ -std=c++17 -I/opt/homebrew/Cellar/boost/1.87.0/include boost1.cpp -o boost1 -g 
 #include <boost/multiprecision/cpp_int.hpp>
 using namespace boost::multiprecision;
 using namespace std;
